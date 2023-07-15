2a0a424328e3612866626fd2c2eb3419d43a9bfb
CyberGnida <ivan.mol.10@mail.ru>
2023-07-15 16:15:33 +0300

    computed functions + .sync user event

--- index.js
+++ index.js
@@ -1,20 +1,61 @@
+Vue.component('like-button', {
+    props: ['counter'],
+    template: `<div><button type="button" @click="increment">&#9829; {{ counter }}</button></div>`,
+    methods: {
+        increment() {
+            this.$emit('update:counter', this.counter + 1);
+        },
+        alert(msg) {
+            alert(msg)
+        },
+    },
+})
+
+Vue.component('task-list', {
+    props: ['tasks'],
+    template:
+        `<div class="list">
+      {{ title }}
+      <div class="item" :class="{done: task.done}" v-for="task in tasks" :key="task.text">
+        <input type="checkbox" v-model="task.done">
+        {{ task.text }}
+        <like-button :counter.sync='task.likes'></like-button>
+      </div>
+    </div>`,
+})
+
 let app = new Vue({
     el: '#app',
     data: {
+        headerLikes: 3,
+        formLikes: 5,
         message: 'Add something new?',
         tasks: [
-            {text: 'Learn JavaScript', done: true},
-            {text: 'Learn Vue', done: false},
-            {text: 'Make something cool', done: false},
+            {text: 'Learn JavaScript', done: true, likes: 3},
+            {text: 'Learn Vue', done: false, likes: 5},
+            {text: 'Make something cool', done: false, likes: 0},
         ],
     },
     methods: {
+
         addTask() {
-            this.tasks.push({text: this.message, done: false});
+            this.tasks.push({text: this.message, done: false, likes: 0});
             this.message = '';
         },
+
+    },
+    computed: {
         count() {
             return this.tasks.filter(task => !task.done).length
+        },
+        completedTasks() {
+            return this.tasks.filter(task => task.done)
+        },
+        unCompletedTasks() {
+            return this.tasks.filter(task => !task.done)
+        },
+        countLikes() {
+            return this.headerLikes + this.formLikes + this.tasks.reduce((value, task) => value + task.likes, 0)
         }
     }
 })
